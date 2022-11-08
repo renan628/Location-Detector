0458480ef5b212839bfd0ad76877e25efe1c4cb1
Renan <renanoliveira628@gmail.com>
2022-11-08 12:40:12 -0300

    test: formatting code and adding test to get axios exception

--- src/infra/http/@core/axios/axios.integration.spec.ts
+++ src/infra/http/@core/axios/axios.integration.spec.ts
@@ -1,28 +1,46 @@
 import { IHttpRequest } from "../http.interface";
 import { AxiosHttpRequest } from "./axios";
 import nock from "nock";
+import { AxiosError } from "axios";
 
 describe("Node HTTPS unit test", () => {
-  const fakeApiURL: string = "http://someurl"
+  const fakeApiURL = "http://someurl";
   let axiosRequest: IHttpRequest;
 
   beforeAll(() => {
     axiosRequest = new AxiosHttpRequest(fakeApiURL);
-  })
+  });
 
   it("Should make a get request for a json api", async () => {
-    const fakePath: string = "/somepath";
+    const fakePath = "/somepath";
     const fakeResponse = { result: "OK" };
     const queryParams = {
       param1: "value1",
       param2: "value2",
-    }
+    };
 
-    nock(fakeApiURL)
-    .get(fakePath)
-    .query(queryParams)
-    .reply(200, fakeResponse)
+    nock(fakeApiURL).get(fakePath).query(queryParams).reply(200, fakeResponse);
 
-    await expect(axiosRequest.get(fakePath, queryParams)).resolves.toEqual(fakeResponse);
-  })
-})
+    await expect(axiosRequest.get(fakePath, queryParams)).resolves.toEqual(
+      fakeResponse,
+    );
+  });
+
+  it("Should throw an exception when receives error status code", async () => {
+    const fakePath = "/somepath";
+    const fakeResponse = { result: "OK" };
+    const queryParams = {
+      param1: "value1",
+      param2: "value2",
+    };
+
+    nock(fakeApiURL).get(fakePath).query(queryParams).reply(500, fakeResponse);
+
+    await expect(axiosRequest.get(fakePath, queryParams)).rejects.toMatchObject(
+      {
+        message: expect.stringContaining("500"),
+        cause: expect.any(AxiosError),
+      },
+    );
+  });
+});
